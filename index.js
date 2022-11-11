0193594b99c4e95dc6630bb9bd7ac6574ae7bebc
Bilidam Solomon <solomonbilidam9@gmail.com>
2022-11-11 11:39:32 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,225 @@
+// let countEl = document.getElementById("count-el")
+
+// console.log(countEl)
+
+// let count = 0
+
+// function increment(){
+//     count = count + 1
+//     countEl.innerText = count
+    
+// }
+
+
+
+// console.log("solomon is the legendary man #")
+
+// let x =  10 +  19 + " vove"  
+// console.log(x)
+
+//  function solomon(a, b, c){
+//     return a * c + b
+//  }
+
+//   console.log(solomon(9, 3, 5))
+//   console.log(solomon( 9, 9, 9))
+
+
+//   let myName = ("solomon")
+//   console.log(myName)
+
+
+//   function convertionToInteger(str){
+//     return parseInt(str, 2);
+//   }
+
+//    var a = convertionToInteger("10011");
+//    console.log(a)
+
+//    function checkSign(num){
+//     return num > 0 ? "positive" : num < 0 ? " negative" : "zero"
+//    }
+
+//     console.log(checkSign(0));
+
+
+//     console.log("hello world")
+
+
+
+
+
+    
+
+
+    // a = 9 % 4;
+    // console.log(a)
+    // Math.random()
+    // console.log("what's up bro? ")
+
+    //  function solomon(a, b) {
+    //    return a + b
+    //  }
+
+
+
+//  max = 0 
+
+// let numbers = [ 7, 6, 7,  90]
+// for(let num of numbers){
+// if (num > max){
+//     max = num
+//   }
+//   // console.log( max)
+// }
+
+// console.log(max)
+
+
+
+// console.log("heyy")
+// names = {
+
+//     solo: "legend",
+//     david: "greattttt",
+//     woo: "woooo"
+// }
+
+// console.log(names["woo"])
+// console.log(names.david)
+
+
+ 
+// var names = [ 2, 4, 5, 6, ];
+ 
+// console.log(names.join("")
+
+
+// let now = new Date();
+// console.log(now)
+
+
+// now.getDate()
+
+
+// //let email = new RexExp 
+// let num = new RegExp("z")
+
+// let mum = 'how are u'
+
+// console.log(num.test( mum))
+
+// let arr = [ 2, 1, 3, 4, 5, ]
+// let mumm = arr.push( 7, 8, 9)  
+// console.log(mumm)
+
+
+// let results = [4, 5, 6, 8].map(number => number * 2)
+// console.log(results)
+
+
+
+// let num  = (number) => {
+//     return number.map(number => number + 2)
+// } 
+//  console.log(num([2, 3, 4, ]))
+
+//  let hey = ( numbers, greater) => {
+//     let results = []
+//     for(let yoo of numbers){
+//         if( yoo > greater){
+//             results.push(yoo)
+//         }
+//     }
+//     return results
+//  }
+// console.log(hey([ 4, 5, 6, 7,], 3))
+
+
+// let myfirstProject = [
+//   ' wooo', ' legend', ' legendary solomon']
+// console.log('myfirstProject')
+
+/*console.log('solomon is a greate man')
+function colors(){
+    let colour =['red', 'yellow', 'green', 'black', 'orange']
+    let randomIndex = Math.floor((Math.random() * colour.length))
+     let randomColor = colors[randomIndex]
+    console.log(randomColor)
+    
+    
+  }
+   colors()
+   */                 
+____________________________________________________________________
+//    class Rectangle {
+//     constructor(_height, _width, _color){
+//       console.log('my name is solomon');
+      
+//       this.width = _width;
+//       this.height = _height;
+//       this.color = _color;
+//     }
+    
+//     Area(){
+//       return this.width * this.height
+//     }
+//     printDescription(){
+//       console.log(`i am a rectangle of ${this.width} x ${this.height}  and i am ${this.color}`);
+//     }
+    
+//   }
+  
+//    let mm = new Rectangle(3, 5, 'blue')
+//    let nn2 = new Rectangle( 10, 15, 'yellow')
+  
+//   console.log(nn2.printDescription())
+____________________________________________________________________
+
+
+
+
+// console.log('hello you')
+
+// class person{
+//   constructor( _name, _age){
+//     this.name = _name
+//     this.age =_age
+//   }
+ 
+// } 
+// var solo = new person('solomon', 19)
+// console.log(solo)
+
+
+
+
+
+
+// if(isKofi )  ? "Kofi is a boy":"Kofi is not a boy" 
+
+
+
+
+
+
+
+
+
+// function numbers( num1, num2, num3){
+//     if( num1 > num2){
+//       return ('number one is greater than number two')
+//     } else if ( num3 < num2){
+//       return  ('i am very much fine')
+//     } else{ return ('i am fine')}
+    
+//   }
+    
+//     console.log(numbers(90, 85, 99))
+
+
+
+
+// function alertUser(){
+//   alert('well done, solomon');
+// }
